5d95d5a79d821e065c06e7a72c4466dceea0bab2
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-03 23:27:34 +0200

    Add files via upload

---
+++ Decorator/TypesDecoratorBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+   public abstract class TypesDecoratorBase: IWindow
+    {
+        protected IWindow window;
+        public abstract string GetDetails();
+
+        public TypesDecoratorBase(IWindow window)
+        {
+            this.window = window;
+        }
+
+    }
+}
