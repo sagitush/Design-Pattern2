f4e7f449102f4503ef65b8a4886bd650dcd66815
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-03 23:24:49 +0200

    Add files via upload

---
+++ Decorator/Shadowing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Shadowing: TypesDecoratorBase
+    {
+        public Shadowing(IWindow window) : base(window)
+        {
+        }
+
+        public override string GetDetails()
+        {
+            return window.GetDetails() + " Shadowing";
+        }
+    }
+}
