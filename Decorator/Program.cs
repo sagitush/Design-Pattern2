e9b2b6331f1e24696eddbc3f9623948ed5f788c6
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-03 23:24:12 +0200

    Add files via upload

---
+++ Decorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWindow baseWindow = new BaseWindow();
+            IWindow threeD = new ThreeDLighting(baseWindow);
+            IWindow shadow = new Shadowing(threeD);
+            //Console.WriteLine(shadow.GetDetails());
+           
+
+           IWindow colrfulShadowingThreeDPartialMirroFlashingBase= new ColorfulFrame(new Shadowing(new ThreeDLighting(new PartialMirroring(new BackgroundFlashing(new BaseWindow())))));
+           Console.WriteLine(colrfulShadowingThreeDPartialMirroFlashingBase.GetDetails());
+         
+
+        }
+    }
+}
