fe7ab8fc8881431323fb947644e16f9b4ca210d5
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-03 23:28:25 +0200

    Add files via upload

---
+++ Proxy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RealCar car = new RealCar();
+            GetProxy(new CarProxy());
+
+            car.SlowDown();
+            car.StartDriving();
+            car.StopDriving();
+            car.IncreaseSpeed();
+            car.TurnLeft();
+            car.TurnRight();
+        }
+
+        static void GetProxy(IMonitorService proxy)
+        {
+            Console.WriteLine(proxy.ShowFuelMode());
+            Console.WriteLine(proxy.ShowLocation());
+        }
+    }
+ }
+
