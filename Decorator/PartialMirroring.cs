dd844527e0bc640dfab5da17d08adc0c0cad19a3
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-03 23:23:52 +0200

    Add files via upload

---
+++ Decorator/PartialMirroring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class PartialMirroring:TypesDecoratorBase
+    {
+        public PartialMirroring(IWindow window) : base(window)
+        {
+        }
+
+        public override string GetDetails()
+        {
+            return window.GetDetails() + " Partial mirroring";
+
+        }
+    }
+}
