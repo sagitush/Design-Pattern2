be8e5d823f726809299d9d6634fe0f328aa253e7
sagitush <44786027+sagitush@users.noreply.github.com>
2020-03-03 23:27:03 +0200

    Add files via upload

---
+++ Decorator/ThreeDLighting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class ThreeDLighting :TypesDecoratorBase
+    {
+        public ThreeDLighting(IWindow window) : base(window)
+        {
+        }
+
+        public override string GetDetails()
+        {
+            return window.GetDetails() + " ThreeD lighting";
+        }
+    }
+}
